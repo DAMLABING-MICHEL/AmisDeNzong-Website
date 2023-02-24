66c5f212f2d3af5952f08267bb99385be4142da2
Damlabing Michel <sylviadamlabing2@gmail.com>
2023-02-24 11:53:32 +0100

    retrieve only somme teachers in AboutController

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Grade;
 use App\Models\Testimonial;
-use Illuminate\Http\Request;
-
+use App\Repositories\StaffRepository;
+use App\Services\StaffService;
 class AboutController extends Controller
 {
+    protected $staffService;
     public function index()
     {
         $highestGrade = Grade::where('hight_grade', true)->get()->first();
-        $certifiedTeachers = ($highestGrade != null) ? (Grade::findOrFail($highestGrade->id))->staffs()->get():null;
+        // $certifiedTeachers = ($highestGrade != null) ? (Grade::findOrFail($highestGrade->id))->staffs()->get():null;
+        $this->staffService = new StaffService(new StaffRepository);
+        $certifiedTeachers = $this->staffService->getCertifiedTeachers();
         $testimonials = Testimonial::where('status','active')->get();
         return view('front.about', compact([
         'testimonials',
